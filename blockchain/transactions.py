ff694b8eebd5c281200fc26efc4f6a0040cac995
Dmitrii Mingalev <d.mingalev99@gmail.com>
2019-10-31 14:37:28 +0300

    Add verify transaction

--- blockchain/transactions.py
+++ blockchain/transactions.py
@@ -32,3 +32,37 @@ def createTransaction(recipient, amount):
     # TODO: Дописать создание транзакции!
 
     return transaction
+
+
+def verifyTransaction(transaction):
+    """
+    Метод для проверки транзакции.
+    Принимает транзакцию и проверяет её. Если всё ок, то возвращает True.
+    """
+    hash = transaction.get('hash')
+    sender = transaction.get('sender')
+    recipient = transaction.get('recipient')
+    amount = transaction.get('amount')
+    signature = transaction.get('signature')
+
+    # TODO: Добавить проверку баланса!
+
+    try:
+        pubkey = base64.b64decode(sender).decode('utf-8')
+        pubkey = rsa.PublicKey.load_pkcs1(pubkey)
+    except:
+        return False
+
+    signature = base64.b64decode(signature)
+
+    new_hash = str(sender) + str(recipient) + str(amount)
+    new_hash = new_hash.encode('utf-8')
+    new_hash = hashlib.md5(new_hash).hexdigest()
+
+    if hash != new_hash:
+        return False
+
+    if rsa.verify(hash.encode('utf-8'), signature, pubkey) != 'SHA-1':
+        return False
+
+    return True
